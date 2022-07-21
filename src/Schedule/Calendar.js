7c78ca8c22f9788f3d5d2e50c2f09cd1ee3900f9
Kaschae Freeman <kaschaefreeman@gmail.com>
2022-07-21 19:47:45 -0400

    update calendar functionality

--- src/Schedule/Calendar.js
+++ src/Schedule/Calendar.js
@@ -1,55 +1,113 @@
 import React, { useEffect, useState } from "react";
 
 const Calendar = () => {
-const [viewMonth, setViewMonth] = useState()
+  const [viewDate, setViewDate] = useState(new Date());
+  const [matches, setMatches] = useState(
+    window.matchMedia("(min-width: 391px)").matches
+  );
+  const weekdays = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  const monthNames = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
 
-document.body.style = "background: white; color: black";
+  let row1 = [];
+  let row2 = [];
+  let row3 = [];
+  let row4 = [];
+  let row5 = [];
+  let row6 = [];
 
- const date = new Date();
-  date.setDate(1);
-  const firstDay = date.getDay();
-  const year = date.getFullYear()
-  const month = date.getMonth();
-  const monthEnd = new Date(year,month+1,0)
+  document.body.style = "background: white; color: black";
 
-    const calendarRows = [];
-    let row1 = [];
-    let row2 = [];
-    let row3 = [];
-    let row4 = [];
-    let row5 = [];
-    let row6 = [];
+  useEffect(() => {
+    window
+      .matchMedia("(min-width: 391px)")
+      .addEventListener("change", (e) => setMatches(e.matches));
+  }, []);
+
+  viewDate.setDate(1);
+  const firstDay = viewDate.getDay();
+  const year = viewDate.getFullYear();
+  const month = viewDate.getMonth();
+  const monthEnd = new Date(year, month + 1, 0);
+  const calendarRows = [];
 
   for (let n = 1; n < 7; n++) {
     let row = eval(`row${n}`);
     for (let i = n * 7 - 7; i < 7 * n; i++) {
-      const day = i - firstDay+1;
+      const day = i - firstDay + 1;
       let element;
       if (day < 1 || day > monthEnd.getDate()) {
         element = <td></td>;
       } else {
-        element = <td>{day}</td>;
+        element = <td className="calendarDate">{day}</td>;
       }
       row.push(element);
     }
-    calendarRows.push((<tr key={n}>{row}</tr>));
+    calendarRows.push(<tr key={n}>{row}</tr>);
   }
 
+  const tableHeadings = weekdays.map((day) => {
+    const label = matches ? day : day[0];
+    return <th>{label}</th>;
+  });
+
+  const handleIncrease =() =>{
+    const d = new Date()
+    d.setMonth(month+1);
+    d.setDate(1)
+    setViewDate(d)
+  }
+    const handleDecrease = () => {
+      const d = new Date();
+      d.setMonth(month + 1);
+      d.setDate(1);
+      setViewDate(d);
+    };
   return (
-    <table className="table table-striped table-light">
-      <thead>
-        <tr>
-          <th scope="col">Sunday</th>
-          <th scope="col">Monday</th>
-          <th scope="col">Tuesday</th>
-          <th scope="col">Wednesday</th>
-          <th scope="col">Thursday</th>
-          <th scope="col">Friday</th>
-          <th scope="col">Saturday</th>
-        </tr>
-      </thead>
-      <tbody>{calendarRows}</tbody>
-    </table>
+    <div className="card text-center">
+      <div className="card-header">Select a Day</div>
+      <div className="card-body">
+        <div className="card-title container">
+          <div className="row">
+            <div className="col-3">
+              <button className="btn" onClick={handleDecrease}>{`<`}</button>
+            </div>
+            <div className="col-6">
+              <h3 onClick={() => setViewDate(new Date())}>{monthNames[month]}</h3>
+            </div>
+            <div className="col-3">
+              <button className="btn" onClick={handleIncrease}>{`>`}</button>
+            </div>
+          </div>
+        </div>
+        <table className="table  ">
+          <thead>
+            <tr>{tableHeadings}</tr>
+          </thead>
+          <tbody>{calendarRows}</tbody>
+        </table>
+      </div>
+    </div>
   );
 };
 export default Calendar;
