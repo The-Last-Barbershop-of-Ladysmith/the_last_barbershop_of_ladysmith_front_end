db8098e36beb173db7e40a22368e3cc10fab4289
Kaschae Freeman <kaschaefreeman@gmail.com>
2023-01-13 22:37:55 -0500

    clean up inline css and unnecessary files Also added comments to home files

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -5,26 +5,29 @@ import './Home.css';
 import { motion } from "framer-motion"
 
 const Home = () => {
-  const welcomePrompt = (
-    <motion.div className='welcome' initial={{ opacity: 2, zIndex: 2 }} animate={{
-      opacity: 0, transitionEnd: { zIndex: -1 }
-    }} transition={{ duration: 1.5 }}>
 
+  const welcomePrompt = (
+    <motion.div 
+      className='welcome' 
+      initial={{ opacity: 2, zIndex: 2 }} 
+      animate={{
+        opacity: 0, transitionEnd: { zIndex: -1 }
+      }} 
+      transition={{ duration: 1.5 }}
+      >
       <img src={logo} alt="The Last Barbershop" />
       <h2 className='display-4'>Welcome to the Last Barbershop</h2>
-      
-
     </motion.div>
   )
   const mainScreen = (
-    <div className='mainScreen '>
-      <div className='blur d-flex flex-column justify-content-center'>
-      <h2 className='display-3'>The Last Barbershop of Ladysmith, VA</h2>
-      <h3>Haircuts, and styles for the whole family</h3>
-      <Link to='/schedule' className=" scheduleBtn btn btn-secondary m-5 justify-self-end">
-        <span className="material-symbols-outlined">calendar_month</span>
-        <span> Schedule An Appointment</span>
-      </Link>
+    <div className='mainScreen'>
+      <div className='blur'>
+        <h2 className='display-3'>The Last Barbershop of Ladysmith, VA</h2>
+        <h3>Haircuts, and styles for the whole family</h3>
+        <Link to='/schedule' className=" scheduleBtn btn btn-secondary m-5 justify-self-end">
+          <span className="material-symbols-outlined">calendar_month</span>
+          <span> Schedule An Appointment</span>
+        </Link>
       </div>
     </div>
   )
